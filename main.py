8c0c6a1a2405910b55932bd4af7f4ae8ea384e80
unknown <danla2178@gmail.com>
2023-03-26 13:15:13 +0300

    Расширил и добавил main.py в репозиторий

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from random import choices
 
 symbols = 'ABCDEFGHJKLMNOPQRSTUVWXYZ'
+symbolslow = 'abcdefghjklmnopqrstuvwxyz'
 
 chars = ''
 
@@ -10,6 +11,7 @@ pwd_auto = (input('автоматически подобрать пароль? (
 
 for text, seq in(
                  ('Включить верхний регистор', symbols),
+                 ('Включить нижний регистор', symbolslow),
     if pwd_auto or(input(text + ' (y,n): ') == 'y'):
        chars += seq
 password = ''.join(choices(chars, k=pwd_length))
